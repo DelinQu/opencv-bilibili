ff92117e78ba9b22afce9d941d0bd296d6c8755c
DelinQu <qdl.cs@qq.com>
2021-12-03 22:41:53 +0800

    add LK

--- assignments/optical-flow/LKOpticalFlow.py
+++ assignments/optical-flow/LKOpticalFlow.py
@@ -26,13 +26,43 @@ class LKOpticalFlow:
     def LKvideo(self,vpath):
         cap = cv.VideoCapture(vpath)
         ret, old_frame = cap.read()
-        old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+        prevImg = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 
-        p0 = cv.goodFeaturesToTrack(old_gray, mask=None, **self.feature_params)
+        prevPts = cv.goodFeaturesToTrack(prevImg, mask=None, **self.feature_params)
         # Create a mask image for drawing purposes
         mask = np.zeros_like(old_frame)
 
         # Create some random colors
         color = np.random.randint(0, 255, (100, 3))
 
+        while(True):
+            ret, frame = cap.read()
+            if(not ret):
+                break
+            nextImg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            nextPts, status, err = cv.calcOpticalFlowPyrLK(prevImg,nextImg,prevPts,None,**self.lk_params)
 
+            # Select good points
+            if nextPts is not None:
+                good_old = prevPts[status == 1]
+                good_new = nextPts[status == 1]
+
+            # draw the tracks
+            for i,(old,new) in enumerate(zip(good_old,good_new)):
+                xo, yo = old
+                xn, yn = new
+
+                mask = cv.line(mask, (int(xo), int(yo)), (int(xn), int(yn)), color[i].tolist(), 2)
+                frame = cv.circle(frame, (int(xn), int(yn)), 5, color[i].tolist(), -1)
+
+            img = cv.add(frame, mask)
+            cv.imshow('frame', img)
+            k = cv.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+            # Now update the previous frame and previous points
+            prevImg = nextImg.copy()
+            prevPts = good_new.reshape(-1, 1, 2)
+
+        cv.destroyAllWindows()
--- assignments/optical-flow/main.py
+++ assignments/optical-flow/main.py
@@ -1,5 +1,3 @@
-import cv2.cv2 as cv
-import numpy as np
 import argparse
 from LKOpticalFlow import LKOpticalFlow
 
@@ -9,14 +7,4 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
 
     LK = LKOpticalFlow()
-
-    # img = cv.imread('/home/qdl/Pictures/head2.jpg')
-    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    #
-    # sift = cv.SIFT_create()
-    # kp = sift.detect(gray, None)
-    # sift_img = cv.drawKeypoints(gray, kp, img)
-    # LK.cv_show('orignal',img)
-    #
-    # corners = cv.goodFeaturesToTrack(gray, 25, 0.01, 10)
-    # sift_img = cv.drawKeypoints(gray, corners, img.copy())
+    LK.LKvideo(args['video'])
